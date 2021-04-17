5fa1d9e8e70f66be0ed5b9e202e3d1b2ff760883
MrPurple666 <antoniosacramento666usa@gmail.com>
2021-04-17 01:06:05 +0000

    Add initial miatoll vendorsetup.sh
    this vendorsetup.sh clone hardware_xiaomi from lineageOS, clone proton-clang from kdragon and clone miuicamera for miatoll

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-18.1 hardware/xiaomi 
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera -b master vendor/xiaomi/miuicamera
+
