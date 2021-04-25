37a427b91272b540a5a8ef3ee2a72d31daebb997
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-04-25 05:41:04 +0000

    add other Broken rules and proton

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,7 @@
 export BUILD_BROKEN_DUP_RULES=true
 export BUILD_BROKEN_PHONY_TARGETS=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS=true
 export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+export TARGET_KERNEL_CLANG_VERSION=proton
