bbb90a8fc3f016f694bb5c143b72253f937b9074
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-04-26 04:32:45 +0000

    Delete Gapps, Vanilla is besss

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,4 @@ export ALLOW_MISSING_DEPENDENCIES=true
 export SKIP_ABI_CHECKS=true
 git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 export TARGET_KERNEL_CLANG_VERSION=proton
+rm -rf vendor/gapps
