cc710db1bca1d8fb6d7097af30d27162b5459db6
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-04-29 20:00:56 +0000

    add audio hals export
    idk if this works

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,4 @@ export TARGET_KERNEL_CLANG_VERSION=proton
 rm -rf vendor/gapps
 export BOARD_USES_QCOM_HARDWARE=true 
 export TARGET_BOARD_PLATFORM=msm8998
+export SRC_AUDIO_HAL_DIR=hardware/qcom-caf/msm8998/audio  
