783321c02d0644c1d50993fcbdbe114f712014c7
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-04-30 03:27:21 +0000

    Bruh it's really not work :v

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,4 +8,3 @@ export TARGET_KERNEL_CLANG_VERSION=proton
 rm -rf vendor/gapps
 export BOARD_USES_QCOM_HARDWARE=true 
 export TARGET_BOARD_PLATFORM=msm8998
-export SRC_AUDIO_HAL_DIR=hardware/qcom-caf/msm8998/audio  
