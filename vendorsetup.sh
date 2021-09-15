dea57d8f04cc40c66b6690bd0f896c78c3d3dce5
Mr_Purple_666 <78635632+MrPurple666@users.noreply.github.com>
2021-09-15 02:46:02 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-18.1 hardware/xiaomi 
 git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
-git clone https://github.com/SonalSingh18/vendor_xiaomi_miuicamera -b master vendor/xiaomi/miuicamera
+git clone https://github.com/MrPurple666/vendor_xiaomi_miuicamera -b master vendor/xiaomi/miuicamera
 
 export BOARD_USES_QCOM_HARDWARE=true 
 export TARGET_BOARD_PLATFORM=sm6250
